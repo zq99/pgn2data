14af7fc140ee183b110fe8517e933d064e164ecb
zq99 <9282913+zq99@users.noreply.github.com>
2021-07-26 00:00:36 +0100

    Changed version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ classifiers = [
 
 setup(
     name='pgn2data',
-    version='0.0.3',
+    version='0.0.4',
     packages=['converter', 'common', 'testing'],
     url='',
     classifiers=classifiers,
